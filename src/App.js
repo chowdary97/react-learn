3930ac8477fa43996824bcb0c5589e3ea0a539d1
Thimma Chowdary <thimmachowdary1997@gmail.com>
2024-07-15 21:01:55 +0530

    episode-2 igniting app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 // const heading = React.createElement('h1',{id:'heading'},"Hello Thimma") // second param is the object of attributes like id, classname and title for the heading
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(heading);
+import React from 'react';
+import ReactDOM from 'react-dom'
 
 const parent = React.createElement('div', { id: 'parent' }, [React.createElement('h1', { id: "child" }, "hello world"), React.createElement('h2', { id: "child" }, "hello world")]);
 

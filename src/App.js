93d6452c8f5fa3857184067ac0db0ff0f7b58083
Thimma Chowdary <thimmachowdary1997@gmail.com>
2024-07-15 17:12:06 +0530

    react core

---
+++ src/App.js
@@ -0,0 +1,9 @@
+// const heading = React.createElement('h1',{id:'heading'},"Hello Thimma") // second param is the object of attributes like id, classname and title for the heading
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(heading);
+
+const parent = React.createElement('div', { id: 'parent' }, [React.createElement('h1', { id: "child" }, "hello world"), React.createElement('h2', { id: "child" }, "hello world")]);
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(parent);
